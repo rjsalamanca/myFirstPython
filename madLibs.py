921539ff9f22bfed123c3789805fd5712f0be2f5
RJ Salamanca <salamancaronito@gmail.com>
2019-04-16 15:49:54 -0400

    added more text

--- madLibs.py
+++ madLibs.py
@@ -1,5 +1,8 @@
 name = input('What is your name: ')
 language = input('What programming language are you learning: ')
 food = input('What did you eat for lunch: ')
+name2 = input('Who is sitting next to you: ')
+food2 = input('What did you eat last night: ')
+adj = input('Adjective: ')
 
-print ('%s is learning %s. Today %s ate %s and it was delicious.' % (name,language,name,food))
+print ('\n%s is learning %s. Today %s ate %s and it was delicious. %s saw %s eating and said they were a messy eater. %s was %s and threw %s at %s.\n' % (name,language,name,food,name2,name,name2,adj,food2,name))
