5d4540c398556e9e1f56480f19b335f8defd47fd
RJ Salamanca <salamancaronito@gmail.com>
2019-04-17 11:02:47 -0400

    function now returns print statement

--- madLibsExercise/madLibs.py
+++ madLibsExercise/madLibs.py
@@ -1,5 +1,6 @@
 def make_madlib(name,programming_language,food,name_of_person_beside_you, dinner_last_night,adj):
-    return '\n%s is learning %s. Today %s ate %s and it was delicious. %s saw %s eating and said they were a messy eater. %s was %s and threw %s at %s.\n' % (name,language,name,food,name_of_person_beside_you,name,name_of_person_beside_you,adj,dinner_last_night,name)
+    print('you are calling madlib. thank you for your support')
+    return print('\n%s is learning %s. Today %s ate %s and it was delicious. %s saw %s eating and said they were a messy eater. %s was %s and threw %s at %s.\n' % (name,language,name,food,name_of_person_beside_you,name,name_of_person_beside_you,adj,dinner_last_night,name))
 
 name = input('What is your name: ')
 language = input('What programming language are you learning: ')
@@ -8,5 +9,4 @@ name2 = input('Who is sitting next to you: ')
 food2 = input('What did you eat last night: ')
 adj = input('Adjective: ')
 
-print(make_madlib(name,language,food,name2,food2,adj))
-print('you are calling madlib. thank you for your support')
+make_madlib(name,language,food,name2,food2,adj)
