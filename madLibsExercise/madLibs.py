f14ea2a3078194ac809c3463956105b0112a611b
RJ Salamanca <salamancaronito@gmail.com>
2019-04-17 11:07:51 -0400

    Added a function to capitalize the names passed through make_madlib

--- madLibsExercise/madLibs.py
+++ madLibsExercise/madLibs.py
@@ -1,6 +1,11 @@
 def make_madlib(name,programming_language,food,name_of_person_beside_you, dinner_last_night,adj):
+    name = make_first_caps(name)
+    name_of_person_beside_you = make_first_caps(name_of_person_beside_you)
     print('you are calling madlib. thank you for your support')
     return print('\n%s is learning %s. Today %s ate %s and it was delicious. %s saw %s eating and said they were a messy eater. %s was %s and threw %s at %s.\n' % (name,language,name,food,name_of_person_beside_you,name,name_of_person_beside_you,adj,dinner_last_night,name))
+
+def make_first_caps(make_name_caps):
+    return make_name_caps.capitalize()
 
 name = input('What is your name: ')
 language = input('What programming language are you learning: ')
